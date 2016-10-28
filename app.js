24645d5e7629e75bcef16a74c3ea167f8ed50598
takshingli810 <takshingli810@gmail.com>
2016-10-28 03:26:27 -0700

    got a bit of responsive down

--- app.js
+++ app.js
@@ -1,64 +1,74 @@
 $(function(){
   console.log("loaded!!");
+  //global variable
+  var $hello = $("<h1 id='hello'>Hello World!</h1>"); //For hello animation
 
   //scrolling
   $(window).scroll(function(){
     var wScroll = $(this).scrollTop(); // scrolling var
 
-    $("#name").css({ //scrolling down name
-      'transform' : 'translate(0px, '+ wScroll * 0.4 +'%)'
-    });
+    console.log(wScroll); // console logging where it is scrolling
 
-    $("#macBook").css({ //scrolling macbook down
-      'transform' : 'translate(0px, '+ wScroll * 0.19 +'%)'
-    });
+    //calling functions
+    parallaxIntro();
+    scrollOffset();
 
-    $("#iMac").css({ //scrolling iMac down
-      'transform' : 'translate(0px, '+ wScroll * 0.16 +'%)'
-    });
 
-    $("#iPad").css({ //scrolling ipad down
-      'transform' : 'translate(0px, '+ wScroll * 0.2 +'%)'
-    });
-
-    $("#iPhone").css({ //scrolling iPhone down
-      'transform' : 'translate(0px, '+ wScroll * 0.13 +'%)'
-    });
-
-    if(wScroll <= 10){
-      //tilting iphone back
-      $("#iMac").css({
-        "transform" : "rotateZ(-3deg)"
+    //parallax for intro
+    function parallaxIntro(){
+      $("#name").css({ //scrolling down name
+        'transform' : 'translate(0px, '+ wScroll * 0.4 +'%)'
       });
-      //tilting macbook back
-      $("#macBook").css({
-        "transform" : "rotateZ(4deg)"
+
+      $("#macBook").css({ //scrolling macbook down
+        'transform' : 'translate(0px, '+ wScroll * 0.2 +'%)'
       });
-      //tiling iPad back
-      $("#iPad").css({
-        "transform" : "rotateZ(-4deg)"
+
+      $("#iPhone").css({ //scrolling iPhone down
+        'transform' : 'translate(0px, '+ wScroll * 0.24 +'%)'
       });
-      //tiling iPhone back
-      $("#iPhone").css({
-        "transform" : "rotateZ(3deg)"
-      });
+
+      //resetting rotation for intro
+      if(wScroll <= 50){
+
+        $("#macBook").css({
+          "transform" : "rotateZ(4deg)"
+        });
+
+        $("#iPhone").css({
+          "transform" : "rotateZ(-5deg)"
+        });
+      }
     }
 
-    if(wScroll >= $("#aboutMeDiv").offset().top - ($(window).height() / 1.5)) {
-      $("#iPad").fadeOut("1"); //fading ipad out
-      $("#iPhone").fadeOut("1"); //fading iphone out
-      $("#iMac").fadeOut("1"); //fading iMac out
-      $("#macBook").fadeOut("1"); //fading macBook out
+    //offset animation
+    function scrollOffset() {
+      if (wScroll >= $("#aboutMeDiv").offset().top - ($(window).height() * 0.5)) {
+        $("#macBook").hide("1"); //fading macBook out
+        $("#iPhone").hide("1"); //fading iphone out
+        $("#name").fadeOut("4000");
+
+        $($hello).appendTo("#aboutMeDiv")
+                 .animate({
+                   top: '400px',
+                   left: '500px',
+                   opacity: '1',
+                   height: '1000px',
+                   width: '1000px',
+                   fontSize: '100px'
+                 }, 500);
+      }
+      else if (wScroll < $("#aboutMeDiv").offset().top - ($(window).height() / 8)) {
+        $("#macBook").fadeIn("slow");
+        $("#iPhone").fadeIn("slow");  //fading iphone back in
+        $("#name").fadeIn("slow");
+        $("#hello").remove();
+
+
+      }
     }
 
-    if(wScroll === 0) {
-      $("#iPad").fadeIn("slow");  //fading ipad back in
-      $("#iPhone").fadeIn("slow");  //fading iphone back in
-      $("#iMac").fadeIn("slow");
-      $("#macBook").fadeIn("slow");
-    }
-
-
-    console.log(wScroll); // console logging where it is scrolling can comment out later
-  });
 });
+
+
+}); //closing bracket for onload
